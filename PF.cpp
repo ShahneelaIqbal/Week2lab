7958487b082c2dc9e10da6c8853039c1bb39d558
DESKTOP-LEJPUHO <shahneelaiqbal1@gmail.com>
2022-12-16 21:52:25 +0500

    code

---
+++ PF.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+
+
+main(){
+cout << "My name is Shahneela Iqbal";
+
+cout << endl;
+
+cout << "PF Lab 3";
+
+cout << endl;
+
+cout << "My section is C" << endl;
+
+cout << "My roll number is 20";
+
+
+}
---
+++ tictac.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+main(){
+
+cout << "   |  |   " << endl;
+cout << " 0 |1 |1  " << endl;
+cout << " __|__|__ " << endl;
+cout << "   |  |   " << endl;
+cout << " 1 |0 |   " << endl;
+cout << " __|__|__ " << endl;
+cout << "   |  |   " << endl;
+cout << "   |  | 0 ";
+
+}
